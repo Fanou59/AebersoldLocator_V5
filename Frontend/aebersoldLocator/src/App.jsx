e18484280669cd752ad40805ff3a0054946fe71f
Ste[PH]ane <steph.guery@gmail.com>
2024-11-17 21:58:43 +0100

    Ajout de la configuration de Tailwind CSS, intégration des styles dans l'application et amélioration de l'interface utilisateur avec des composants réactifs

--- Frontend/aebersoldLocator/src/App.jsx
+++ Frontend/aebersoldLocator/src/App.jsx
@@ -4,7 +4,10 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div>
+    <div className="p-4">
+      <header class="bg-indigo-800 text-white p-4 font-russo text-3xl">
+        Aebersold Locator
+      </header>
       <SearchBar />
     </div>
   );
@@ -23,8 +26,16 @@ function SearchBar() {
   };
 
   return (
-    <div>
-      <input type="text" placeholder="Search..." onChange={handleChange} />
+    <div className="bg-white border border-gray-200 rounded-lg shadow-md p-6">
+      <label for="search" class="font-roboto text-sm text-gray-700">
+        Nom du morceau
+      </label>
+      <input
+        type="text"
+        placeholder="Enter a title..."
+        onChange={handleChange}
+        className="border border-gray-200 rounded p-2 mt-2 w-full"
+      />
       <SearchResults title={title} />
     </div>
   );
@@ -37,19 +48,34 @@ function SearchResults({ title }) {
   if (isError) return <div>Error...</div>;
 
   return (
-    <div>
-      {data.length > 0 ? (
-        data.map((item) => (
-          <div key={item.id}>
-            <h3>{item.title}</h3>
-            <p>Key: {item.key}</p>
-            <p>Style: {item.style}</p>
-            <p>Tempo: {item.tempo}</p>
-          </div>
-        ))
-      ) : (
-        <div>Aucun résultat trouvé.</div>
-      )}
+    <div className="p-6">
+      <div className="bg-white border border-gray-200 rounded-lg shadow-md p-4 mb-4">
+        <h3 className="font-roboto text-lg font-semibold text-gray-800">
+          Morceau recherché
+        </h3>
+        <div className="mt-4">
+          {data.length > 0 ? (
+            data.map((item) => (
+              <div className="p-4 border-b border-gray-200" key={item.id}>
+                <h3 className="font-roboto font-medium text-gray-900">
+                  {item.title}
+                </h3>
+                <p className="font-roboto text-sm text-gray-600">
+                  Key: {item.key}
+                </p>
+                <p className="font-roboto text-sm text-gray-600">
+                  Style: {item.style}
+                </p>
+                <p className="font-roboto text-sm text-gray-600">
+                  Tempo: {item.tempo}
+                </p>
+              </div>
+            ))
+          ) : (
+            <div>Aucun résultat trouvé.</div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
